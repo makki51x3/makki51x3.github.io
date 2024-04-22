812cb240804e678f77653d793d47226dc015a2ee
Mohammad Makki <makki.51x3@gmail.com>
2024-04-22 14:41:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('generateBtn').addEventListener('click', function() {
+    fetch('https://v2.jokeapi.dev/joke/Any?type=single')
+        .then(response => response.json())
+        .then(data => {
+            if(data.error) {
+                document.getElementById('roastDisplay').innerText = "Failed to fetch a roast.";
+            } else {
+                document.getElementById('roastDisplay').innerText = data.joke;
+            }
+        })
+        .catch(error => {
+            document.getElementById('roastDisplay').innerText = "Error fetching a roast.";
+        });
+});
